2c58b2697885ae92a4cf49302b788978d4326275
Vladimir <maostyle@mail.ru>
2022-07-07 21:03:35 +0300

    сумма с вводом

---
+++ selfexample/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите 1е число");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите 2е число");
+int numberB = int.Parse(Console.ReadLine());
+int result = numberA + numberB;
+Console.Write("Результат=");
+Console.WriteLine(result);
